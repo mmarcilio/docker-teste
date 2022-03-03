5270d4ef5190b0bb573cc6ae33cae13ab8502b0b
mmarcilio <martheusmacilio1@gmail.com>
2022-03-03 13:27:30 -0300

    add dockerfile

---
+++ php/www/index.php
@@ -0,0 +1,4 @@
+<?php 
+  echo "<pre><h2>Se apareceu isso, deu bom.</h2></pre><br/>";
+  mysqli_connect("db", "root", "root") or die(mysqli_error()); 
+  echo "<pre><h3>Conectou no Mysql<h3></pre><br/>";
